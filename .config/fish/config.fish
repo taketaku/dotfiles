411983261b3c1fed3bad8fdbab4236453c0bbbd7
taketaku <vbvbsp48@gmail.com>
2020-02-02 19:12:31 +0900

    fisher add bobthefish-them

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,5 @@
 set -x PATH $HOME/.anyenv/bin $PATH
-
 anyenv init - fish | source
+
+# vi mode
+fish_vi_key_bindings
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 ln -fns ~/dotfiles/.bashrc ~/.bashrc
+ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
 ln -fns ~/dotfiles/.config/fish ~/.config/fish
 ln -fns ~/dotfiles/.config/fusuma ~/.config/fusuma
-ln -fns ~/dotfiles/.gitconfig ~/.gitconfig
+ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
