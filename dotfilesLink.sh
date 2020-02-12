de1261450938a896ac490d36be80b04c24897937
taketaku <vbvbsp48@gmail.com>
2020-02-12 20:02:34 +0900

    add task sample

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,4 +6,5 @@ ln -fns ~/dotfiles/.config/fusuma ~/.config/fusuma
 ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
 ln -fns ~/dotfiles/.config/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 ln -fns ~/dotfiles/.config/vscode/settings.json ~/.config/Code/User/settings.json
+ln -fns ~/dotfiles/.config/vscode/tasks.json ~/.config/Code/User/tasks.json
 ln -fns ~/dotfiles/.config/byobu ~/.config/byobu
