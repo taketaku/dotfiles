cea2f2cae19fbbc25482899f2f48acd6f6e3ea3a
taketaku <vbvbsp48@gmail.com>
2020-07-21 00:45:46 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,4 +7,5 @@ ln -fns ~/dotfiles/.config/nvim ~/.config/nvim
 ln -fns ~/dotfiles/.config/vscode/keybindings.json ~/.config/Code/User/keybindings.json
 ln -fns ~/dotfiles/.config/vscode/settings.json ~/.config/Code/User/settings.json
 ln -fns ~/dotfiles/.config/vscode/tasks.json ~/.config/Code/User/tasks.json
+ln -fns ~/dotfiles/.config/vscode/snippets ~/.config/Code/User/snippets
 ln -fns ~/dotfiles/.config/byobu ~/.config/byobu
